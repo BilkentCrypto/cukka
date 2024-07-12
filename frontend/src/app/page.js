0fde5c76937187076f0d1bd4c88232052e635a1a
Kaanayden <kaanayden15@gmail.com>
2024-07-12 22:27:18 +0200

    Initialize AppKit

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import ConnectButton from "../components/ui/ConnectButton";
 
 export default function Home() {
   return (
@@ -8,7 +9,9 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/app/page.js</code>
         </p>
+
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <ConnectButton/>
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
