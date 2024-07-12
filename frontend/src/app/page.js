85c3196232ff430d54a529fdeb2333a8e9cc520f
nasuh dincer <a12346552@univie.ac.at>
2024-07-13 01:18:06 +0200

    github

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,10 +1,22 @@
+'use client';
 import Image from "next/image";
 import ConnectButton from "../components/ui/ConnectButton";
 
+
+const CLIENT_ID = "Ov23li9nq1LDqOZ6BXmj"
+
 export default function Home() {
+
+  function loginwithGithub(){
+    window.location.assign("https://github.com/login/oauth/authorize?client_id=" + CLIENT_ID)
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <ConnectButton/>
+      <button onClick={loginwithGithub}>
+        Login with github
+      </button>
     </main>
   );
 }
