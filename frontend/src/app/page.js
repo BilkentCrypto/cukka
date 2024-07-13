97741ca9744b11ce5eee79c81cebe8494a30d568
Aegean <egeaybars1102@gmail.com>
2024-07-13 10:48:30 +0200

    process env & remove node modules

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -3,19 +3,17 @@ import Image from "next/image";
 import ConnectButton from "@/components/ui/ConnectButton";
 import { Button } from "@/components/ui/button";
 
-
-
 const CLIENT_ID = "Ov23li9nq1LDqOZ6BXmj"
 
 export default function Home() {
 
-  function loginwithGithub(){
-    window.location.assign("https://github.com/login/oauth/authorize?client_id=" + CLIENT_ID)
+  function loginwithGithub() {
+    window.location.assign("https://github.com/login/oauth/authorize?client_id=" + process.env.NEXT_PUBLIC_CLIENT_ID)
   }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ConnectButton/>
+      <ConnectButton />
       <button onClick={loginwithGithub}>
         Login with github
       </button>
