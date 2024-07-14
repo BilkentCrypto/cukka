877689b68b13a5c74a16492c496b06f63772ca5c
Kaanayden <kaanayden15@gmail.com>
2024-07-14 08:22:11 +0200

    Fix logs

--- frontend/src/app/(main)/my-accounts/page.js
+++ frontend/src/app/(main)/my-accounts/page.js
@@ -26,45 +26,8 @@ import { config } from "@/config";
 
 export default function Accounts() {
 
-  const client = usePublicClient(config);
-  const {data} = useBlockNumber();
-  console.log("client: ", client)
 
-  const [logs, setLogs] = useState([])
 
-  const setup = async () => {
-    const logs = await client.getLogs({  
-      
-      address: contractAddresses.ESCROW_CONTRACT_ADDRESS,
-      event: parseAbiItem('event FundsDeposited(string indexed, uint256, bool)'),
-      args: {
-        username: "egeaybars1233",
-      },
-      fromBlock: "earliest",
-      toBlock: "latest",
-    }, [data])
-    setLogs(logs);
-  }
-
-   useEffect( ()=> {
-setup();
-   }, [data, client] );
-console.log("logs: ", logs)
-  
-  const transactions = [
-    {
-      user: "John Doe",
-      username: "@johndoe",
-      platform: "twitter",
-      amount: "50.00",
-    },
-    {
-      user: "Jane Smith",
-      username: "@janesmith",
-      platform: "github",
-      amount: "75.00",
-    },
-  ];
 
   const handleClaimFunds = () => {
     // Logic to claim funds
@@ -85,26 +48,8 @@ console.log("logs: ", logs)
           <ClaimButton onClick={handleClaimFunds} className="py-3 px-6 text-lg" />
         </div>
       </main>
-      <footer className=" text-gray-700 py-8">
-        <div className="container mx-auto px-4 md:px-6">
-          <h2 className="text-2xl font-bold mb-6 text-center">Transaction History</h2>
-          <div className="rounded-lg border shadow-lg p-4 bg-white mx-4">
-            <Table className="w-full">
-              <TableHeader>
-                <TableRow>
-                  <TableHead>User</TableHead>
-                  <TableHead className="text-center">Social Media</TableHead>
-                  <TableHead className="text-right">Amount</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {transactions.map((transaction, index) => (
-                  <TransactionRow key={index} transaction={transaction} />
-                ))}
-              </TableBody>
-            </Table>
-          </div>
-        </div>
+      <footer className="text-center text-3xl text-gray-700 py-4">
+        And many more...
       </footer>
     </div>
   );
